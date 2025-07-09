567dd8fa5ecb9d35bb3ca9919017d92f7834f605
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-07-09 18:55:01 -0300

    init project

---
+++ src/test/java/daniel/PaymentProcessor/PaymentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.PaymentProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

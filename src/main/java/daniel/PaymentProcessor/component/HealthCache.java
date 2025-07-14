936be3233268d6c0be8f2d72084c266b203f0e58
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-07-14 18:25:16 -0300

    feat: init func to save cache and process

---
+++ src/main/java/daniel/PaymentProcessor/component/HealthCache.java
@@ -0,0 +1,10 @@
+package daniel.PaymentProcessor.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCache {
+    // Coletar templates
+    // Adicionar urls
+    // Salvar em cache
+}
---
+++ src/main/java/daniel/PaymentProcessor/config/WebClientConfig.java
@@ -0,0 +1,8 @@
+package daniel.PaymentProcessor.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
+    // COnfigure restTemplate
+}
